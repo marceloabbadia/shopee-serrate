cf326afcf8560a378cd6360c55670c77e57e253a
FelipeLEopoldino <felipeleopoldino01@icloud.com>
2023-06-09 14:28:09 -0300

    Rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import ListaProdutos from "../src/pages/listaProdutos/ListaProdutos";
-import Header from "./components/header/Header";
-import Login from "../src/pages/login/Login";
-import Cadastro from "./pages/cadastro/Cadastro";
+import ListaProdutos from '../src/pages/listaProdutos/ListaProdutos'
+import Header from './components/header/Header'
+import Login from '../src/pages/login/Login'
+import Cadastro from './pages/cadastro/Cadastro'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
-  return (
-    <>
-      <Header />
-      {/* <ListaProdutos /> */}
-      <Login />
-      {/* <Cadastro /> */}
-    </>
-  );
+   return (
+      <>
+         <Header />
+         <BrowserRouter>
+            <Routes>
+               <Route path="/" element={<ListaProdutos />} />
+               <Route path="/login" element={<Login />} />
+               <Route path="/cadastro" element={<Cadastro />} />
+            </Routes>
+         </BrowserRouter>
+      </>
+   )
 }
 
-export default App;
+export default App
