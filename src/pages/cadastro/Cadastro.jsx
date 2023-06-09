7faf30f00c3aa8a000dc53b37a2cf0e96109daf8
Abbadia Desenvolvimento <abbadiadev@MacBook-Pro-de-Marcelo.local>
2023-06-08 21:14:27 -0300

    concluido logica JS de cadastro, do login e criacao de token Strin com 28 caracteres

--- src/pages/cadastro/Cadastro.jsx
+++ src/pages/cadastro/Cadastro.jsx
@@ -1,35 +1,147 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import api from "../../api/api";
 import styles from "./cadastro.module.css";
 
 const Cadastro = () => {
+  const [nome, setNome] = useState([]);
+  const [cpf, setCpf] = useState([]);
+  const [telefone, setTelefone] = useState([]);
+  const [email, setEmail] = useState([]);
+  const [senha, setSenha] = useState([]);
+  const [confSenha, setConfSenha] = useState([]);
+
+  const handleChangeNome = (e) => {
+    setNome(e.target.value);
+  };
+  const handleChangeCpf = (e) => {
+    setCpf(e.target.value);
+  };
+
+  const handleChangeTelefone = (e) => {
+    setTelefone(e.target.value);
+  };
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangeSenha = (e) => {
+    setSenha(e.target.value);
+  };
+
+  const handleChangeConfSenha = (e) => {
+    setConfSenha(e.target.value);
+  };
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+
+    if (senha != confSenha) {
+      alert("Senha e confirmacao de senha nao sao iguais, tente novamente!");
+      setSenha("");
+      setConfSenha("");
+    } else {
+      let listaUsuarios = JSON.parse(
+        localStorage.getItem("listaUsuarios") || "[]"
+      );
+      listaUsuarios.push({
+        Nome: nome,
+        Cpf: cpf,
+        Telefone: telefone,
+        Email: email,
+        Senha: senha,
+      });
+
+      localStorage.setItem("listaUsuarios", JSON.stringify(listaUsuarios));
+
+      alert("Usuario cadastrado com sucesso!");
+
+      resetarForm();
+    }
+  };
+
+  const resetarForm = () => {
+    setNome("");
+    setCpf("");
+    setTelefone("");
+    setEmail("");
+    setSenha("");
+    setConfSenha("");
+  };
+
   return (
     <>
       <div>
-        <form className={styles.form_cadastro}>
+        <form className={styles.form_cadastro} onSubmit={handleSubmitForm}>
           <h2>CADASTRO</h2>
-          <input type="text" required />
+
+          <input
+            value={nome}
+            type="text"
+            id="nome"
+            required
+            onChange={handleChangeNome}
+          />
           <p></p>
-          <label htmlFor="">Nome (completo) </label>
+          <label htmlFor="nome">Nome e Sobrenome </label>
           <br />
-          <input type="number" required />
+          <input
+            value={cpf}
+            type="number"
+            id="cpf"
+            required
+            onChange={handleChangeCpf}
+          />
           <p></p>
-          <label htmlFor="">CPF (apenas números) </label>
+          <label htmlFor="cpf">CPF (apenas números) </label>
           <br />
-          <input type="email" required />
+          <input
+            value={telefone}
+            type="number"
+            id="telefone"
+            required
+            onChange={handleChangeTelefone}
+          />
           <p></p>
-          <label htmlFor="">Email </label>
+          <label htmlFor="telefone">Telefone </label>
           <br />
-          <input type="password" required />
+          <input
+            value={email}
+            type="email"
+            id="email"
+            required
+            onChange={handleChangeEmail}
+          />
           <p></p>
-          <label htmlFor="">Senha </label>
+          <label htmlFor="email">Email </label>
           <br />
-          <input type="password" required />
+          <br />
+          <input
+            value={senha}
+            type="password"
+            id="senha"
+            required
+            onChange={handleChangeSenha}
+          />
           <p></p>
-          <label htmlFor="">Confirmar Senha </label>
+          <label htmlFor="senha">Senha </label>
           <br />
-          <button>Cadastrar</button>
-          <button>Limpar</button>
+          <br />
+          <input
+            value={confSenha}
+            type="password"
+            id="confSenha"
+            required
+            onChange={handleChangeConfSenha}
+          />
+          <p></p>
+          <label htmlFor="confSenha">Confirmar Senha </label>
+          <br />
+          <br />
+          <button type="submit">Cadastrar</button>
+          <button type="reset" onClick={resetarForm}>
+            Limpar
+          </button>
           <br />
           <br />
           <button className={styles.btn_voltar}>Voltar</button>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,23 +1,89 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import api from "../../api/api";
 import styles from "./login.module.css";
 
 const Login = () => {
+  const [emailLogin, setEmailLogin] = useState([]);
+  const [senhaLogin, setSenhaLogin] = useState([]);
+
+  const handleChangeEmailLogin = (e) => {
+    setEmailLogin(e.target.value);
+  };
+
+  const handleChangeSenhaLogin = (e) => {
+    setSenhaLogin(e.target.value);
+  };
+
+  const handleSubmitFormLogin = (e) => {
+    e.preventDefault();
+
+    if (emailLogin === "" || senhaLogin === "") {
+      alert("Não é possível logar com um campo vazio");
+    } else {
+      let listaUsuarios = [];
+
+      let userValid = {
+        email: "",
+        senha: "",
+      };
+
+      listaUsuarios = JSON.parse(localStorage.getItem("listaUsuarios"));
+
+      listaUsuarios.forEach((item) => {
+        if (emailLogin == item.Email && senhaLogin == item.Senha) {
+          userValid = {
+            email: item.email,
+            senha: item.senha,
+          };
+        }
+      });
+
+      if (userValid.email == "" || userValid.senha == "") {
+        alert("Senha ou login inválidos!");
+        resetarFormLogin();
+      } else {
+        let token =
+          Math.random().toString(16).substring(2) +
+          Math.random().toString(16).substring(2);
+        localStorage.setItem("token", token);
+        alert("Seu token foi validado e voce será redirecionado!");
+        resetarFormLogin();
+      }
+    }
+  };
+
+  const resetarFormLogin = () => {
+    setEmailLogin("");
+    setSenhaLogin("");
+  };
+
   return (
     <>
       <div>
-        <form className={styles.form_login}>
+        <form className={styles.form_login} onSubmit={handleSubmitFormLogin}>
           <h3>LOGIN</h3>
-          <input type="email" required />
+          <input
+            value={emailLogin}
+            type="email"
+            required
+            onChange={handleChangeEmailLogin}
+          />
           <p></p>
-          <label htmlFor="">Email: </label>
+          <label htmlFor="emailLogin">Email: </label>
           <br />
-          <input type="password" required />
+          <input
+            value={senhaLogin}
+            type="password"
+            required
+            onChange={handleChangeSenhaLogin}
+          />
           <p></p>
-          <label htmlFor="">Senha: </label>
+          <label htmlFor="senhaLogin">Senha: </label>
           <br />
-          <button>Enviar</button>
-          <button>Limpar</button>
+          <button type="submit">Entrar</button>
+          <button type="reset" onClick={resetarFormLogin}>
+            Limpar
+          </button>
           <br />
           <br />
           <p>Nao é cadastrado?</p>
