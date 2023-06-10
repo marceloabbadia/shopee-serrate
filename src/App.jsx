7911cd799375b0f129afff1cecc239dff9d32264
marceloabbadia <marcelo.abbadia@gmail.com>
2023-06-10 12:10:20 -0300

    Alteracoes feita na pagina de usuario/login (criada nova pasta para mudanca)

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import Header from "./components/header/Header";
 import Login from "../src/pages/login/Login";
 import Cadastro from "./pages/cadastro/Cadastro";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginEUsuario from "./pages/logineusuario/Logineusuario";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Header />
-        <Routes>
+        <LoginEUsuario />
+        {/* <Routes>
           <Route path="/" element={<ListaProdutos />} />
           <Route path="/login" element={<Login />} />
           <Route path="/cadastro" element={<Cadastro />} />
-        </Routes>
+        </Routes> */}
       </BrowserRouter>
     </>
   );
---
+++ src/pages/logineusuario/Logineusuario.jsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+// import Visibility from "@mui/icons-material/Visibility";
+// import VisibilityOff from "@mui/icons-material/VisibilityOff";
+// import Visibility from "@mui/icons-material/Visibility";
+
+function InputAdornments() {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  return (
+    <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+      <div>
+        <TextField
+          label="With normal TextField"
+          id="outlined-start-adornment"
+          sx={{ m: 1, width: "25ch" }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">kg</InputAdornment>
+            ),
+          }}
+        />
+        <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+          <OutlinedInput
+            id="outlined-adornment-weight"
+            endAdornment={<InputAdornment position="end">kg</InputAdornment>}
+            aria-describedby="outlined-weight-helper-text"
+            inputProps={{
+              "aria-label": "weight",
+            }}
+          />
+          <FormHelperText id="outlined-weight-helper-text">
+            Weight
+          </FormHelperText>
+        </FormControl>
+        <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+      </div>
+    </Box>
+  );
+}
+export default InputAdornments;
