66f8376b75acffc20505100e69fe65aaabcc6153
FelipeLEopoldino <felipeleopoldino01@icloud.com>
2023-06-12 21:32:52 -0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
-import ListaProdutos from "../src/pages/listaProdutos/ListaProdutos";
-import Header from "./components/header/Header";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import DetalheProduto from "./pages/detalheProduto/DetalheProduto";
-import LoginEUsuario from "./pages/loginEUsuario/logineusuario";
-import Carrinho from "./pages/carrinho/Carrinho";
-import { Context } from "./context/Context";
-import PesquisaProdutos from "./pages/pesquisaprodutos/PesquisaProdutos";
+import ListaProdutos from '../src/pages/listaProdutos/ListaProdutos'
+import Header from './components/header/Header'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import DetalheProduto from './pages/detalheProduto/DetalheProduto'
+import LoginEUsuario from './pages/loginEUsuario/logineusuario'
+import Carrinho from './pages/carrinho/Carrinho'
+import PesquisaProdutos from './pages/pesquisaprodutos/PesquisaProdutos'
+import Footer from './components/footer/Footer'
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<ListaProdutos />} />
-          <Route path="/login" element={<LoginEUsuario />} />
-          <Route path="/detalheProduto/:id" element={<DetalheProduto />} />
-          <Route path="/pesquisaProdutos/" element={<PesquisaProdutos />} />
-          <Route path="/carrinho" element={<Carrinho />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+   return (
+      <>
+         <BrowserRouter>
+            <Header />
+            <Routes>
+               <Route path="/" element={<ListaProdutos />} />
+               <Route path="/login" element={<LoginEUsuario />} />
+               <Route path="/detalheProduto/:id" element={<DetalheProduto />} />
+               <Route
+                  path="/pesquisaProdutos/"
+                  element={<PesquisaProdutos />}
+               />
+               <Route path="/carrinho" element={<Carrinho />} />
+            </Routes>
+            <Footer />
+         </BrowserRouter>
+      </>
+   )
 }
 
-export default App;
+export default App
