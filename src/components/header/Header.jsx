8bc2925e9cf0c05824afdd3243470ba936eea665
marceloabbadia <marcelo.abbadia@gmail.com>
2023-06-13 23:46:36 -0300

    ajustadas as tealas de pesquisa por nome e alguns css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,55 +6,26 @@ import { RiLoginBoxLine } from "react-icons/ri";
 import { AiOutlineClear } from "react-icons/ai";
 import { useState, useContext } from "react";
 import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
 import Box from "@mui/material/Box";
-import api from "../../api/api";
 import PesquisaContext from "../../context/PesquisaContext";
-import { borderRadius } from "@mui/system";
 
 const Header = () => {
   const { searchValue, setSearchValue } = useContext(PesquisaContext);
-  // const [searchValue, setSearchValue] = useState("");
 
   const handleSearch = (event) => {
     event.preventDefault();
     setSearchValue("");
   };
-import styles from './header.module.css'
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { BsSearch, BsCart3 } from 'react-icons/bs'
-import { useState } from 'react'
-import TextField from '@mui/material/TextField'
-import Autocomplete from '@mui/material/Autocomplete'
-import Box from '@mui/material/Box'
-import api from '../../api/api'
 
-const Header = () => {
-   const [searchValue, setSearchValue] = useState('')
-
-   const handleSearch = async event => {
-      event.preventDefault()
-      const response = await api.get(`/produtos?title=${searchValue}`)
-      setSearchValue(response.data)
-   }
-
-   const pesquisaMenu = [
-      { label: 'Masculino' },
-      { label: 'Feminino' },
-      { label: 'Joalheria' },
-      { label: 'Informática' }
-   ]
-
-   return (
-      <>
-         <div className={styles.header}>
-            <nav className={styles.nav}>
-               <div className={styles.title}>
-                  <Link to={'/'}>
-                     <h1>SHOPEE SERRATEC</h1>
-                  </Link>
-               </div>
+  return (
+    <>
+      <div className={styles.header}>
+        <nav className={styles.nav}>
+          <div className={styles.title}>
+            <Link to={"/"}>
+              <h1>SHOPEE SERRATEC</h1>
+            </Link>
+          </div>
 
           <form className={styles.pesquisa_nome} onSubmit={handleSearch}>
             <Box
@@ -83,73 +54,23 @@ const Header = () => {
               </button>
             </Link>
           </form>
-
           <Link to="/carrinho">
             <button className={styles.btn_carrinho}>
               <BsCart3 />
             </button>
           </Link>
-               <form className={styles.pesquisa_nome} onSubmit={handleSearch}>
-                  <Box
-                     sx={{
-                        width: 500,
-                        maxWidth: '80%',
-                        bgcolor: 'white',
-                        boxShadow: 1,
-                        borderRadius: 3
-                     }}
-                  >
-                     <TextField
-                        fullWidth
-                        value={searchValue}
-                        label="Pesquisar por nome"
-                        onChange={({ target }) => setSearchValue(target.value)}
-                     />
-                  </Box>
-                  <button type="submit">
-                     <BsSearch />
-                  </button>
-               </form>
-
-               <div className={styles.pesquisa_categoria}>
-                  <Autocomplete
-                     disablePortal
-                     id="combo-box-demo"
-                     options={pesquisaMenu}
-                     sx={{
-                        width: 250,
-                        bgcolor: 'white',
-                        boxShadow: 1,
-                        borderRadius: 3
-                     }}
-                     renderInput={params => (
-                        <TextField {...params} label="Pesquisar Categoria" />
-                     )}
-                  />
-               </div>
-
-               <Link to="/carrinho">
-                  <button>
-                     <BsCart3 />
-                  </button>
-               </Link>
-
-               <br />
-
+          <br />
           <Link to="/login">
             <button className={styles.btn_login}>
               <RiLoginBoxLine />
             </button>
           </Link>
-               <Link to="/login">
-                  <button>Login </button>
-               </Link>
 
-               <br />
-            </nav>
-         </div>
-      </>
-   )
-}
+          <br />
+        </nav>
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/listaProdutos/ListaProdutos.jsx
+++ src/pages/listaProdutos/ListaProdutos.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useContext, useEffect, useState } from "react";
 import api from "../../api/api";
 import styles from "./listaProdutos.module.css";
@@ -6,25 +5,17 @@ import { Link } from "react-router-dom";
 import { BsHandThumbsDown, BsHandThumbsUp } from "react-icons/bs";
 import { CarrinhoContext } from "../../context/Context";
 import PesquisaContext from "../../context/PesquisaContext";
-=======
-import { useContext, useEffect, useState } from 'react'
-import api from '../../api/api'
-import styles from './listaProdutos.module.css'
-import { Link } from 'react-router-dom'
-import { BsHandThumbsDown, BsHandThumbsUp } from 'react-icons/bs'
-import { CarrinhoContext } from '../../context/Context'
-import ThumbDownIcon from '@mui/icons-material/ThumbDown';
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import { ButtonGroup, Button } from '@mui/material'
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import Checkbox from '@mui/material/Checkbox';
-import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
-import Favorite from '@mui/icons-material/Favorite';
-import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
+import ThumbDownIcon from "@mui/icons-material/ThumbDown";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import { ButtonGroup, Button } from "@mui/material";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import Checkbox from "@mui/material/Checkbox";
+import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
+import Favorite from "@mui/icons-material/Favorite";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
 
-const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
->>>>>>> 1ba329d455539243846108c09fc23f74ae829bd3
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 const ListaProdutos = () => {
   const [data, setData] = useState([]);
@@ -46,7 +37,6 @@ const ListaProdutos = () => {
     data();
   }, [searchValue]);
 
-  
   const incrementarFeedbackPositivo = (index) => {
     const incrementar = [...data];
     incrementar[index].feedbacksPositivos++;
@@ -63,138 +53,61 @@ const ListaProdutos = () => {
   const GlobalState = useContext(CarrinhoContext);
   const dispatch = GlobalState.dispatch;
 
-<<<<<<< HEAD
   return (
     <>
+      <div className={styles.tudo}></div>
       <div className={styles.container}>
         <div className={styles.card}>
           {data.map((item, index) => {
             item.quantidade = 1;
-            {
-              if (item.amount > 0) {
-                return (
-                  <div key={index} className={styles.itens}>
-                    <img src={item.image} alt={item.description} width={150} />
-                    <h2>{item.title}</h2>
-                    <p>
-                      {item.description.length > 150
-                        ? item.description.substring(0, 150) + "..."
-                        : item.description}
-                    </p>
-                    <h3>R$ {item.price}</h3>
-                    <h4>
-                      Avaliações <br />
-                      {token == null ? (
-                        <div className={styles.avaliacoes}>
-                          <button
-                            onClick={() => incrementarFeedbackPositivo(index)}
-                            disabled
-                          >
-                            <BsHandThumbsUp />
-                            {item.feedbacksPositivos}
-                          </button>
-                          <br />
-                          <button
-                            onClick={() => decrementarFeedbackPositivo(index)}
-                            disabled
-                          >
-                            <BsHandThumbsDown />
-                            {item.feedbacksNegativos}
-                          </button>
-                        </div>
-                      ) : (
-                        <div className={styles.avaliacoes}>
-                          <button
-                            onClick={() => incrementarFeedbackPositivo(index)}
-                          >
-                            <BsHandThumbsUp />
-                            {item.feedbacksPositivos}
-                          </button>
-                          <br />
-                          <button
-                            onClick={() => decrementarFeedbackPositivo(index)}
-                          >
-                            <BsHandThumbsDown />
-                            {item.feedbacksNegativos}
-                          </button>
-                        </div>
-                      )}
-                    </h4>
-                    <Link to={`/detalheProduto/${item.id}`}>
-                      <button>Ver mais</button>
-                    </Link>
-                    <button
-                      onClick={() => dispatch({ type: "ADD", payload: item })}
-                    >
-                      Adicionar carrinho
-                    </button>
+            return (
+              <div key={index} className={styles.itens}>
+                <img src={item.image} alt={item.description} width={80} />
+                <h2>{item.title}</h2>
+                <p>
+                  {item.description.length > 150
+                    ? item.description.substring(0, 150) + "..."
+                    : item.description}
+                </p>
+                <h3>R$ {item.price}</h3>
+                <h4>
+                  Avaliações <br />
+                  <div className={styles.avaliacoes}>
+                    <strong>
+                      <ThumbUpIcon color="success" /> {item.feedbacksPositivos}
+                    </strong>
+                    <br />
+                    <strong>
+                      <ThumbDownIcon color="error" /> {item.feedbacksNegativos}
+                    </strong>
                   </div>
-                );
-              }
-            }
+                </h4>
+                <Link to={`/detalheProduto/${item.id}`}>
+                  <Button variant="outlined">VER MAIS</Button>
+                </Link>
+                <p>⠀⠀</p>
+                <Button
+                  className="vermais"
+                  variant="contained"
+                  endIcon={<AddShoppingCartIcon />}
+                  onClick={() => dispatch({ type: "ADD", payload: item })}
+                >
+                  Adicionar ao carrinho
+                </Button>
+                <div className={styles.favorito}>
+                  <Checkbox
+                    {...label}
+                    icon={<FavoriteBorder />}
+                    checkedIcon={<Favorite color="error" />}
+                  />
+                </div>
+              </div>
+            );
           })}
         </div>
       </div>
     </>
   );
 };
-=======
-   return (
-      <>
-         <div className={styles.tudo}>
-            
-         </div>
-         <div className={styles.container}>
-            <div className={styles.card}>
-               {data.map((item, index) => {
-                  item.quantidade = 1
-                  return (
-                     <div key={index} className={styles.itens}>
-                        <img
-                           src={item.image}
-                           alt={item.description}
-                           width={80}
-                        />
-                        <h2>{item.title}</h2>
-                        <p>
-                           {item.description.length > 150
-                              ? item.description.substring(0, 150) + '...'
-                              : item.description}
-                        </p>
-                        <h3>R$ {item.price}</h3>
-                        <h4>
-                           Avaliações <br />
-                           <div className={styles.avaliacoes}>
-                              <strong>
-                                 <ThumbUpIcon color="success" /> {item.feedbacksPositivos}
-                              </strong>
-                              <br />
-                              <strong>
-                                 <ThumbDownIcon color="error" /> {item.feedbacksNegativos}
-                              </strong>
-                           </div>
-                        </h4>
-                        <Link to={`/detalheProduto/${item.id}`}>
-                           <Button variant="outlined">VER MAIS</Button>
-
-                        </Link>
-                        <p>⠀⠀</p>
-                        <Button className="vermais" variant="contained" endIcon={<AddShoppingCartIcon />}
-                           onClick={() =>
-                              dispatch({ type: 'ADD', payload: item })
-                           }>Adicionar ao carrinho</Button>
-                        <div className={styles.favorito}>
-                        <Checkbox {...label} icon={<FavoriteBorder />} checkedIcon={<Favorite color="error" />} />
-                        </div>
-                     </div>
-                     
-                  )
-               })}
-            </div>
-         </div>
-      </>
-   )
-}
->>>>>>> 1ba329d455539243846108c09fc23f74ae829bd3
 
 export default ListaProdutos;
